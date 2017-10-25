1b7edf0baa218b53237da70e09cbf3b8fe0c3486
compman2408 <rstutz2408@gmail.com>
2017-10-24 20:24:37 -0400

    Started Nmap integration testing. This is currently at 1 out of 10 on completeness.

--- app/src/main/java/com/example/blackwidow/ActivityNmapTest.java
+++ app/src/main/java/com/example/blackwidow/ActivityNmapTest.java
@@ -6,6 +6,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class ActivityNmapTest extends Activity {
     private static final String TAG = "ActivityNmapTest";
@@ -28,7 +35,39 @@ public class ActivityNmapTest extends Activity {
     }
 
     public void btnRunNmap_OnClick(View view) {
-        _lblDisplay.setText(_lblDisplay.getText() + "nmap " + _txtNmapParameters.getText() + "\n" + LINE_BEGINNING);
-        _txtNmapParameters.setText("");
+        String filesDir = getApplicationContext().getFilesDir().getPath();
+//        InputStream test = this.getResources().openRawResource(R.raw.nmap);
+        File file = new File(filesDir + "/res/raw");
+        _lblDisplay.setText("Current File: " + file.toString() + "\nExists: " + String.valueOf(file.exists()));
+//        _lblDisplay.setText(_lblDisplay.getText() + "nmap " + _txtNmapParameters.getText() + "\n" + LINE_BEGINNING);
+//        _txtNmapParameters.setText("");
+//        try {
+//            // Executes the command.
+//            Process process = Runtime.getRuntime().exec("/system/bin/ls " + filesDir);
+//
+//            // Reads stdout.
+//            // NOTE: You can write to stdin of the command using
+//            //       process.getOutputStream().
+//            BufferedReader reader = new BufferedReader(
+//                    new InputStreamReader(process.getInputStream()));
+//            int read;
+//            char[] buffer = new char[4096];
+//            StringBuffer output = new StringBuffer();
+//            while ((read = reader.read(buffer)) > 0) {
+//                output.append(buffer, 0, read);
+//            }
+//            reader.close();
+//
+//            // Waits for the command to finish.
+//            process.waitFor();
+//
+//            _lblDisplay.setText(output.toString());
+//        } catch (IOException e) {
+//            Toast.makeText(this, "IOException ERROR: " + e.getMessage(), Toast.LENGTH_LONG).show();
+//        } catch (InterruptedException e) {
+//            Toast.makeText(this, "InterruptedException ERROR: " + e.getMessage(), Toast.LENGTH_LONG).show();
+//        } catch (Exception e) {
+//            Toast.makeText(this, "Exception ERROR: " + e.getMessage(), Toast.LENGTH_LONG).show();
+//        }
     }
 }
