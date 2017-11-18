d57be5ba40dea05d066d26d3683e50148075efe8
Ryan Stutzman <rstutz2408@gmail.com>
2017-11-18 05:35:00 -0500

    Added more of a CLI interface type thing to the test activity. Also made it so that the files get copied at app start and make them executable as well using chmod. Fixed a bug in the file copy process. Still fails to execute on my phone though...

--- app/src/main/java/com/example/blackwidow/ActivityMain.java
+++ app/src/main/java/com/example/blackwidow/ActivityMain.java
@@ -3,16 +3,25 @@ package com.example.blackwidow;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.StateListDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ActivityMain extends Activity {
     private static final String TAG = "ActivityMain";
+
+    private String _appDataDirectory;
+    public static Map<Integer, String> binaryFilesToCopy;
+    public static String binaryFileLocation;
 
     Button btnScanNetwork;
     AnimationDrawable btnAnimation;
@@ -43,6 +52,20 @@ public class ActivityMain extends Activity {
 
         btnAnimation = new AnimationScan(this, R.drawable.btn_scan_frame0);
         btnScanNetwork.setBackground(btnAnimation);
+
+        // Set the directories to the important stuff
+        _appDataDirectory = getApplicationContext().getFilesDir().getPath();
+        binaryFileLocation = _appDataDirectory + "/bin";
+        // Add files to copy
+        binaryFilesToCopy = new HashMap<Integer, String>();
+        binaryFilesToCopy.put(R.raw.ncat, binaryFileLocation + "/ncat");
+        binaryFilesToCopy.put(R.raw.ndiff, binaryFileLocation + "/ndiff");
+        binaryFilesToCopy.put(R.raw.nmap2, binaryFileLocation + "/nmap");
+        binaryFilesToCopy.put(R.raw.nping, binaryFileLocation + "/nping");
+        binaryFilesToCopy.put(R.raw.uninstall_ndiff, binaryFileLocation + "/uninstall_ndiff");
+
+        // Copy the files out of the raw resources so they can be executed
+        copyAllBinaryFiles();
     }
 
     @Override
@@ -57,6 +80,65 @@ public class ActivityMain extends Activity {
         super.onPause();
         if (btnAnimation.isRunning())
             btnAnimation.stop();
+    }
+
+    private void copyAllBinaryFiles() {
+        // If the binary files don't currently exist copy them out of the raw resources onto the
+        // internal storage of the device since the external storage won't allow us to run the
+        // executable on the Android system
+        Log.d(TAG, "Checking binary directory existance...");
+        File file = new File(binaryFileLocation);
+        if (!file.exists()) {
+            Log.d(TAG, "Binary directory doesn't exist. Creating directories...");
+            // Create binary directory
+            file.mkdirs();
+            Log.d(TAG, "Directories created. Copying files...");
+        } else {
+            Log.d(TAG, "Binary directory already exists. Moving on to files.");
+        }
+        Log.d(TAG, "Checking binary files existance...");
+        int count = 0;
+        for (Map.Entry<Integer, String> item : binaryFilesToCopy.entrySet()) {
+            count++;
+            file = new File(item.getValue());
+            if (!file.exists()) {
+                Log.d(TAG, "Binary " + count + " out of " + binaryFilesToCopy.size() + " doesn't exist.");
+                try {
+                    Log.d(TAG, "Copying binary " + count + " out of " + binaryFilesToCopy.size());
+                    copyBinFilesToInternalStorage(item.getKey(), item.getValue());
+                    Log.d(TAG, "File copy successful!");
+                } catch (IOException ex) {
+                    Log.d(TAG, "Error copying file: " + ex.getMessage());
+                    ex.printStackTrace();
+                }
+                try {
+                    Log.d(TAG, "Making binary executable...");
+                    Process process = Runtime.getRuntime().exec("chmod oug+x " + item.getValue());
+                    process.waitFor();
+                    Log.d(TAG, "Binary executable!");
+                } catch (Exception ex) {
+                    Log.d(TAG, "Error marking file as executable: " + ex.getMessage());
+                    ex.printStackTrace();
+                }
+            } else {
+                Log.d(TAG, "Binary " + count + " out of " + binaryFilesToCopy.size() + " already exists. Skipping file copy...");
+            }
+        }
+    }
+
+    private void copyBinFilesToInternalStorage(int id, String path) throws IOException {
+        InputStream in = this.getResources().openRawResource(id);
+        FileOutputStream out = new FileOutputStream(path);
+        byte[] buff = new byte[1024];
+        int read = 0;
+        try {
+            while ((read = in.read(buff)) > 0) {
+                out.write(buff, 0, read);
+            }
+        } finally {
+            in.close();
+            out.close();
+        }
     }
 
     public void btnScanNetwork_OnClick(View view) {
--- app/src/main/java/com/example/blackwidow/ActivityNmapTest.java
+++ app/src/main/java/com/example/blackwidow/ActivityNmapTest.java
@@ -2,115 +2,71 @@ package com.example.blackwidow;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ActivityNmapTest extends Activity {
     private static final String TAG = "ActivityNmapTest";
-    private static final String LINE_BEGINNING = "user@android:~$ ";
+    private static final String LINE_BEGINNING = "{{!user}}@android:~$ ";
+    private String USER = "";
 
-    private Map<Integer, String> _binaryFilesToCopy;
-    private String _appDataDirectory;
-    private String _binaryFileLocation;
 
-    private TextView _lblDisplay;
+    private TextView _lblCLIOutput;
     private EditText _txtNmapParameters;
+    private ScrollView _scrollCLI;
     //private Button _btnRunNmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nmap_test);
-        // Set the directories to the important stuff
-        _appDataDirectory = getApplicationContext().getFilesDir().getPath();
-        _binaryFileLocation = _appDataDirectory + "/bin/nmap";
 
         // Get References to views
-        _lblDisplay = (TextView) findViewById(R.id.lblDisplay);
-        _lblDisplay.setText(LINE_BEGINNING);
+        _lblCLIOutput = (TextView) findViewById(R.id.lblCLIOutput);
         _txtNmapParameters = (EditText) findViewById(R.id.txtNmapParameters);
+        _scrollCLI = (ScrollView) findViewById(R.id.scrollCLI);
         //_btnRunNmap = (Button) findViewById(R.id.btnRunNmap);
 
-        // Add files to copy
-        _binaryFilesToCopy = new HashMap<Integer, String>();
-        _binaryFilesToCopy.put(R.raw.ncat, _binaryFileLocation + "/ncat");
-        _binaryFilesToCopy.put(R.raw.ndiff, _binaryFileLocation + "/ndiff");
-        _binaryFilesToCopy.put(R.raw.nmap2, _binaryFileLocation + "/nmap");
-        _binaryFilesToCopy.put(R.raw.nping, _binaryFileLocation + "/nping");
-        _binaryFilesToCopy.put(R.raw.uninstall_ndiff, _binaryFileLocation + "/uninstall_ndiff");
-
-
-        // If the binary files don't currently exist copy them out of the raw resources onto the
-        // internal storage of the device since the external storage won't allow us to run the
-        // executable on the Android system
-        Log.d(TAG, "Checking binary directory existance...");
-        File file = new File(_binaryFileLocation);
-        if (!file.exists()) {
-            Log.d(TAG, "Binary directory doesn't exist. Creating directories...");
-            // Create binary directory
-            file.mkdirs();
-            Log.d(TAG, "Directories created. Copying files...");
-        } else {
-            Log.d(TAG, "Binary directory already exists. Moving on to files.");
-        }
-        Log.d(TAG, "Checking binary files existance...");
-        int count = 0;
-        for (Map.Entry<Integer, String> item : _binaryFilesToCopy.entrySet()) {
-            count++;
-            file = new File(item.getValue());
-            if (!file.exists()) {
-                Log.d(TAG, "Binary " + count + " out of " + _binaryFilesToCopy.size() + " doesn't exist.");
-                try {
-                    Log.d(TAG, "Copying binary " + count + " out of " + _binaryFilesToCopy.size());
-                    copyBinFilesToInternalStorage(item.getKey(), item.getValue());
-                    Log.d(TAG, "File copy successful!");
-                } catch (IOException ex) {
-                    Log.d(TAG, "Error copying file: " + ex.getMessage());
-                    ex.printStackTrace();
-                }
-            } else {
-                Log.d(TAG, "Binary " + count + " out of " + _binaryFilesToCopy.size() + " already exists. Skipping file copy...");
-            }
-        }
-    }
-
-    private void copyBinFilesToInternalStorage(int id, String path) throws IOException {
-        InputStream in = this.getResources().openRawResource(R.raw.nmap2);
-        FileOutputStream out = new FileOutputStream(path);
-        byte[] buff = new byte[1024];
-        int read = 0;
-        try {
-            while ((read = in.read(buff)) > 0) {
-                out.write(buff, 0, read);
-            }
-        } finally {
-            in.close();
-            out.close();
-        }
+        // Setup the activity
+        USER = executeCommand("whoami").trim();
+        _lblCLIOutput.setText(getCLIBeginning());
     }
 
     public void btnRunNmap_OnClick(View view) {
+        if (_txtNmapParameters.getText().toString().length() == 0) {
+            runCommandInCLI(ActivityMain.binaryFilesToCopy.get(R.raw.nmap2));
+        } else {
+            runCommandInCLI(ActivityMain.binaryFilesToCopy.get(R.raw.nmap2) + " " + _txtNmapParameters.getText().toString());
+        }
+    }
+
+    public void btnTest_OnClick(View view) {
+        runCommandInCLI("/system/bin/ls -l " + ActivityMain.binaryFileLocation);
+    }
+
+    private String getCLIBeginning() {
+        return LINE_BEGINNING.replace("{{!user}}", USER);
+    }
+
+    private void runCommandInCLI(String cmdToExecute) {
+        String cmdOutput = executeCommand(cmdToExecute);
+        _lblCLIOutput.setText(_lblCLIOutput.getText() + cmdToExecute + "\n\n" +  cmdOutput + "\n" + getCLIBeginning());
+        _txtNmapParameters.setText("");
+        _scrollCLI.scrollTo(0, _scrollCLI.getBottom());
+    }
+
+    private String executeCommand(String cmdToExecute) {
+        StringBuffer output = new StringBuffer();
         try {
             // Executes the command.
-            //Process process = Runtime.getRuntime().exec("/system/bin/ls -l " + _binaryFileLocation);
-            Process process;
-            if (_txtNmapParameters.getText().toString().length() == 0) {
-                process = Runtime.getRuntime().exec(_binaryFilesToCopy.get(R.raw.nmap2));
-            } else {
-                process = Runtime.getRuntime().exec(_binaryFilesToCopy.get(R.raw.nmap2) + " " + _txtNmapParameters.getText().toString());
-            }
+            Process process = Runtime.getRuntime().exec(cmdToExecute);
 
             // Reads stdout.
             // NOTE: You can write to stdin of the command using
@@ -119,7 +75,6 @@ public class ActivityNmapTest extends Activity {
                     new InputStreamReader(process.getInputStream()));
             int read;
             char[] buffer = new char[4096];
-            StringBuffer output = new StringBuffer();
             while ((read = reader.read(buffer)) > 0) {
                 output.append(buffer, 0, read);
             }
@@ -128,8 +83,7 @@ public class ActivityNmapTest extends Activity {
             // Waits for the command to finish.
             process.waitFor();
 
-            _lblDisplay.setText(output.toString());
-            Toast.makeText(this, "Done!", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this, "Done!", Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
             Toast.makeText(this, "IOException ERROR: " + e.getMessage(), Toast.LENGTH_LONG).show();
             e.printStackTrace();
@@ -140,5 +94,6 @@ public class ActivityNmapTest extends Activity {
             Toast.makeText(this, "Exception ERROR: " + e.getMessage(), Toast.LENGTH_LONG).show();
             e.printStackTrace();
         }
+        return output.toString();
     }
 }
